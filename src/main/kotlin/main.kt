37ea8b9741a8e2174dff6f11150e20454caf7969
Luan Nico <luanpotter27@gmail.com>
2019-09-08 22:10:07 -0400

    json and all routes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,53 @@
-import spark.kotlin.*
+import com.google.gson.Gson
+import spark.Spark.exception
+import spark.kotlin.Http
+import spark.kotlin.RouteHandler
+import spark.kotlin.halt
+import spark.kotlin.ignite
 
-fun main(args: Array<String>) {
+val json = Gson()
+
+data class Todo(val id: Int, val text: String)
+
+private fun RouteHandler.id() = params("id").toInt()
+
+private fun RouteHandler.parseTodo() = json.fromJson(request.body(), Todo::class.java)
+
+fun main() {
     val http: Http = ignite()
+    val todos: MutableList<Todo> = arrayListOf();
 
-    http.get("/hello") {
-        "Hello Spark Kotlin!"
+    exception(NumberFormatException::class.java) { _, _, _ -> halt(422, "id must be a number") }
+    http.before { type("application/json") }
+
+    http.get("/todos") { json.toJson(todos) }
+    http.get("/todos/:id") {
+        todos.find { e -> e.id == id() }?.let { e -> json.toJson(e) } ?: halt(404)
+    }
+    http.post("/todos/:id") {
+        if (todos.any { e -> e.id == id() }) {
+            halt(422, "Id ${id()} already exists, use PUT to edit")
+        }
+
+        val todo: Todo = parseTodo()
+        todos.add(todo)
+        json.toJson(todo)
+    }
+    http.put("/todos/:id") {
+        val idx: Int = todos.indexOfFirst { e -> e.id == id() }
+        if (idx == -1) {
+            halt(404, "Not found")
+        }
+        val todo = parseTodo()
+        todos[idx] = todo
+        json.toJson(todo)
+    }
+    http.delete("/todos/:id") {
+        val removed = todos.removeIf { e -> e.id == id() }
+        if (removed) {
+            halt(204, "Removed")
+        } else {
+            halt(404, "Not found")
+        }
     }
 }
